7a6e8b053579f7b64d457b8671ac816c8c3e1db6
Alpha2142Gamer <deniss.uniyal@gmail.com>
2018-10-01 17:12:21 +1000

    Added Network Stats
    Added Network Stats

--- volatile.py
+++ volatile.py
@@ -1,5 +1,39 @@
 import os
 import sys
+import subprocess
 from subprocess import call
 
-call([""])
+#Run Date
+print("<=======================DATE==================>")
+call(["date"])
+
+#IP configurations
+print("<=======================IP Configuration==================>")
+call(["ifconfig"])
+
+#Network Statistics
+print("<=======================Netstat==================>")
+call(["netstat"])
+print("\n")
+
+#Last Logged in
+print("<=======================Last Loggedin==================>")
+call(["ps", '-w'])
+print("\n")
+
+#running processes
+print("<=======================Local Processes==================>")
+call(["ps", '-af'])
+print("\n")
+
+#running processes
+print("<=======================System Processes==================>")
+call(["ps", '-Af'])
+print("\n")
+
+#Show startup files
+print("<=======================Startup Files==================>")
+call(["ls", '/etc/init.d'])
+print("\n")
+
+
