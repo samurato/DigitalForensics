434034bcebfe52d911ea20e05edf6a2ecc3ad9e8
Samrat Pant <samrat.sammie@gmail.com>
2018-10-01 19:42:34 +1000

    Added Network Self lookup and requirements.txt

---
+++ network.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import subprocess
+print("<=======================Network Forensics==================>")
+#w = whois.whois('ifconfig.me/ip')
+#print(w)
+some="$(curl -s ifconfig.me/ip)"
+currentIp = "ifconfig.me/ip"
+userIp=subprocess.call(["curl", currentIp])
+sendIp = "$("+ str(userIp) +")"
+subprocess.call (["whois", sendIp])
+print("\n ")
