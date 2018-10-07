0936d8b4bc79eb53e9d16d987781936875c8bd95
Samrat Pant <samrat.sammie@gmail.com>
2018-10-08 00:27:23 +1100

    Fixed Bugs in log file

--- logFile.py
+++ logFile.py
@@ -17,35 +17,33 @@ print("<=======================Copying Authentication Log==================>")
 cwd = os.getcwd()+ "/evidences" 
 selectedFile = Path("/var/log/auth.log")
 if selectedFile.is_file():
-    print("\nCopying /var/log/auth.log to ", cwd + "/logfiles")
-    shutil.copy('/var/log/auth.log', cwd + "/logfiles")
+    print("\nCopying /var/log/auth.log to ", cwd )
+    shutil.copy('/var/log/auth.log', cwd )
 else:
     print("\n /var/log/auth.log doesn't exist")
 
 print("<=======================Copying Mail Log==================>")
-cwd = os.getcwd()
+
 selectedFile = Path("/var/log/mail.log")
 if selectedFile.is_file():
-    print("\nCopying /var/log/mail.log to ", cwd + "/logfiles")
-    shutil.copy('/var/log/mail.log', cwd + "/logfiles")
+    print("\nCopying /var/log/mail.log to ", cwd)
+    shutil.copy('/var/log/mail.log', cwd )
 else:
     print("\n /var/log/mail.log doesn't exist")
 
 print("<=======================mysql Log==================>")
-cwd = os.getcwd()
 selectedFile = Path("/var/log/mysql.log")
 if selectedFile.is_file():
-    print("\nCopying /var/log/mysql.log to ", cwd + "/logfiles")
-    shutil.copy('/var/log/mysql.log', cwd + "/logfiles")
+    print("\nCopying /var/log/mysql.log to ", cwd )
+    shutil.copy('/var/log/mysql.log', cwd)
 else:
     print("\n /var/log/mysql.log doesn't exist")
     
 print("<=======================mysql Log==================>")
-cwd = os.getcwd()
 selectedFile = Path("/var/log/mysql.log")
 if selectedFile.is_file():
-    print("\nCopying /var/log/mysql.log to ", cwd + "/logfiles")
-    shutil.copy('/var/log/mysql.log', cwd + "/logfiles")
+    print("\nCopying /var/log/mysql.log to ", cwd )
+    shutil.copy('/var/log/mysql.log', cwd )
 else:
     print("\n /var/log/mysql.log doesn't exist")
 
@@ -53,25 +51,25 @@ print("<=======================line printer Log==================>")
 cwd = os.getcwd()
 selectedFile = Path("/var/log/lpr.log")
 if selectedFile.is_file():
-    print("\nCopying /var/log/lpr.log to ", cwd + "/logfiles")
-    shutil.copy('/var/log/lpr.log', cwd + "/logfiles")
+    print("\nCopying /var/log/lpr.log to ", cwd)
+    shutil.copy('/var/log/lpr.log', cwd )
 else:
     print("\n /var/log/lpr.log doesn't exist")
     
 print("<=======================line printer Log==================>")
-cwd = os.getcwd()
+
 selectedFile = Path("/var/log/lpr.log")
 if selectedFile.is_file():
-    print("\nCopying /var/log/lpr.log to ", cwd + "/logfiles")
-    shutil.copy('/var/log/lpr.log', cwd + "/logfiles")
+    print("\nCopying /var/log/lpr.log to ", cwd )
+    shutil.copy('/var/log/lpr.log', cwd)
 else:
     print("\n /var/log/lpr.log doesn't exist")
 
 print("<=======================app Log==================>")
-cwd = os.getcwd()
+
 selectedFile = Path("/var/log/syslog")
 if selectedFile.is_file():
-    print("\nCopying /var/log/syslog to ", cwd + "/logfiles")
-    shutil.copy('/var/log/syslog', cwd + "/logfiles")
+    print("\nCopying /var/log/syslog to ", cwd )
+    shutil.copy('/var/log/syslog', cwd )
 else:
     print("\n /var/log/syslog doesn't exist")
