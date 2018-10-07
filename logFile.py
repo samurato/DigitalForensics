9846a9037e4c2c4a196701f70985fec5c84c445e
Alpha2142Gamer <deniss.uniyal@gmail.com>
2018-10-08 00:58:56 +1100

    Log File Fix And Checks

--- logFile.py
+++ logFile.py
@@ -12,11 +12,19 @@ from subprocess import call
 #print(file_contents)
 #print("\n")
 cwd = os.getcwd()+ "/evidences" 
+writeLog = open('evidences/logs.txt', "w+")
+writeLog.write("\n************* Evidence logs ******************\n")
 #Copy non-volatile log files
 print("<=======================Copying Authentication Log==================>")
-f =open('/var/log/auth.log', 'r')
-AuthLog = f.read()
-print(AuthLog)
+my_file = Path(" /var/log/auth.log")
+if my_file.is_file():
+    f =open('/var/log/auth.log', 'r')
+    AuthLog = f.read()
+    print(AuthLog)
+    writeLog.write("\n******************* Authentication Log **************\n")
+    writeLog.write(AuthLog)
+else:
+    print("\n /var/log/auth.log Doesn't Exist")
 # selectedFile = Path("/var/log/auth.log")
 # if selectedFile.is_file():
 #     print("\nCopying /var/log/auth.log to ", cwd )
@@ -25,10 +33,16 @@ print(AuthLog)
 #     print("\n /var/log/auth.log doesn't exist")
 
 print("<=======================Copying Mail Log==================>")
-
-f =open('/var/log/mail.log', 'r')
-mailLog = f.read()
-print(mainLog)
+my_file = Path(" /var/log/mail.log")
+if my_file.is_file():
+    f =open('/var/log/mail.log', 'r')
+    mailLog = f.read()
+    print(mainLog)
+    writeLog.write("\n")
+    writeLog.write("\n****************** Mail Log *********************\n")
+    writeLog.write(mainLog)
+else:
+    print("\n /var/log/mail.log Doesn't Exist")
 # selectedFile = Path("/var/log/mail.log")
 # if selectedFile.is_file():
 #     print("\nCopying /var/log/mail.log to ", cwd)
@@ -37,10 +51,16 @@ print(mainLog)
 #     print("\n /var/log/mail.log doesn't exist")
 
 print("<=======================mysql Log==================>")
-f =open('/var/log/mysql.log', 'r')
-sqlLog = f.read()
-print(sqlLog)
-
+my_file = Path(" /var/log/mysql.log")
+if my_file.is_file():
+    f =open('/var/log/mysql.log', 'r')
+    sqlLog = f.read()
+    print(sqlLog)
+    writeLog.write("\n")
+    writeLog.write("\n***************** mysql Log ************************\n")
+    writeLog.write(sqlLog)
+else:
+    print("\n /var/log/mysql.log Doesn't Exist")
 # selectedFile = Path("/var/log/mysql.log")
 # if selectedFile.is_file():
 #     print("\nCopying /var/log/mysql.log to ", cwd )
@@ -49,9 +69,16 @@ print(sqlLog)
 #     print("\n /var/log/mysql.log doesn't exist")
 
 print("<=======================line printer Log==================>")
-f =open('/var/log/lpr.log', 'r')
-lprLog = f.read()
-print(lprLog)
+my_file = Path(" /var/log/lpr.log")
+if my_file.is_file():
+    f =open('/var/log/lpr.log', 'r')
+    lprLog = f.read()
+    print(lprLog)
+    writeLog.write("\n")
+    writeLog.write("\n***************** line printer Log ************************\n")
+    writeLog.write(lprLog)
+else:
+    print("\n /var/log/lpr.log Doesn't Exist")
 # cwd = os.getcwd()
 # selectedFile = Path("/var/log/lpr.log")
 # if selectedFile.is_file():
@@ -61,10 +88,16 @@ print(lprLog)
 #     print("\n /var/log/lpr.log doesn't exist")
 
 print("<=======================app Log==================>")
-
-f =open('/var/log/syslog', 'r')
-sysLog = f.read()
-print(sysLog)
+my_file = Path(" /var/log/syslog.log")
+if my_file.is_file():
+    f =open('/var/log/syslog', 'r')
+    sysLog = f.read()
+    print(sysLog)
+    writeLog.write("\n")
+    writeLog.write("\n***************** app Log ************************\n")
+    writeLog.write(sysLog)
+else:
+    print("\n /var/log/syslog.log Doesn't Exist")
 # selectedFile = Path("/var/log/syslog")
 # if selectedFile.is_file():
 #     print("\nCopying /var/log/syslog to ", cwd )
@@ -74,21 +107,5 @@ print(sysLog)
 
 ##Spit logs into Text file
 print("\n ")
-writeLog = open('evidences/logs.txt', "w+")
-writeLog.write("\n************* Evidence logs ******************\n")
-writeLog.write("\n******************* Authentication Log **************\n")
-writeLog.write(AuthLog)
-writeLog.write("\n")
-writeLog.write("\n****************** Mail Log *********************\n")
-writeLog.write(mainLog)
-writeLog.write("\n")
-writeLog.write("\n***************** mysql Log ************************\n")
-writeLog.write(sqlLog)
-writeLog.write("\n")
-writeLog.write("\n***************** line printer Log ************************\n")
-writeLog.write(lprLog)
-writeLog.write("\n")
-writeLog.write("\n***************** app Log ************************\n")
-writeLog.write(sysLog)
 writeLog.write("\n")
 writeLog.close()
